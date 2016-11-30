6302fdd0a34fe66941e22b16dd32ab345e854b10
mariusz <pythonms@gmail.com>
2016-11-30 11:00:24 +0100

    session_2

--- chess/session.py
+++ chess/session.py
@@ -1,6 +1,7 @@
 
 
 from player import Player
+from location import Location
 # from mover import Mover()
 
 class Session(object):
@@ -10,9 +11,13 @@ class Session(object):
     def __init__(self):
         self.white = Player(name)
         self.black = Player(name='P2')
+        self.board = Board()
         # self._mover = Mover()
 
-    def do_move(self, player, move):
+    def do_move(self, player, src_spec, dst_spec):
+
+        move = Move(Location(src_spec), Location(dst_spec))
+
         if self.is_white_move:
             self._mover.do_move(self.white, move)
             self.is_white_move = False
