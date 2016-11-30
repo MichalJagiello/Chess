99789cda77a4ac0ad37bdc2cb85a9115b4f9dcc5
Michal Jagiello <michal.jagiello@hotmail.com>
2016-11-30 13:39:47 +0100

    PieceFactory, get_symbol method and tests

--- chess/exceptions.py
+++ chess/exceptions.py
@@ -4,3 +4,10 @@ class IllegalMoveError(Exception):
     """
     Raised when invalid move was requested by a player
     """
+
+
+class InvalidPieceSymbolError(Exception):
+    """
+    Raised when invalid piece symbol
+    was given to piece factory.
+    """
