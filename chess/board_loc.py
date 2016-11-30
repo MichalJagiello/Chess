5d5ecddcb52359423da0fe3321679944fb4dc41a
Jan Kaliszewski <zuo@kaliszewski.net>
2016-11-30 13:56:49 +0100

    board_loc: raise ValueError replaced with assert

--- chess/board_loc.py
+++ chess/board_loc.py
@@ -23,10 +23,13 @@ class Board(object):
         self.set_row_from_symbols('1', 'RNBQKBNR')
 
     def set_row_from_symbols(self, y_label, symbols):
-        if len(symbols) != len(_X_LABELS):
-            raise ValueError('wrong number of symbols: {} (should be {})'.format(
+        assert len(symbols) == len(_X_LABELS), (
+            'wrong number of symbols: {} -- '
+            'should be {} ({!r} vs. {!r})'.format(
                 len(symbols),
-                len(_X_LABELS)))
+                len(_X_LABELS),
+                symbols,
+                _X_LABELS))
         y = _LABEL_TO_Y[y_label]
         self._rows_of_fields[y][:] = [self._piece_factory(s)
                                       for s in symbols]
