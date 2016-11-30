f777eba9413456aa7f422a9128490a7eb86f50a8
mariusz <pythonms@gmail.com>
2016-11-30 11:22:47 +0100

    session_3

--- chess/session.py
+++ chess/session.py
@@ -1,22 +1,24 @@
 
+from collections import namedtuple
 
 from player import Player
 from location import Location
+from board import Board
 # from mover import Mover()
+
 
 class Session(object):
     is_white_move = True
     last_move = None
 
-    def __init__(self):
-        self.white = Player(name)
-        self.black = Player(name='P2')
+    def __init__(self, white_name, black_name):
+        self.white = Player(white_name)
+        self.black = Player(black_name)
         self.board = Board()
         # self._mover = Mover()
 
     def do_move(self, player, src_spec, dst_spec):
-
-        move = Move(Location(src_spec), Location(dst_spec))
+        move = namedtuple(**{'src': Location(src_spec), 'dst': Location(dst_spec)})
 
         if self.is_white_move:
             self._mover.do_move(self.white, move)
