f5a1732e0e2d68bc83796dbaacfaa2b5be104130
Jan Kaliszewski <zuo@kaliszewski.net>
2016-12-01 18:23:33 +0100

    Error improvements/refactoring

--- chess/exceptions.py
+++ chess/exceptions.py
@@ -1,12 +1,12 @@
-class ChessError(Exception):
+class UserError(Exception):
 
     default_msg = None
 
     def __init__(self, msg=None, *args):
         if msg is None:
-            msg = self.default_msg
-        super(ChessError, self).__init__(msg, *args)
+            msg = self.default_msg or 'No, try again...'
+        super(UserError, self).__init__(msg, *args)
 
 
-class IllegalMoveError(ChessError):
+class IllegalMoveError(UserError):
     default_msg = 'The requested move is illegal.'
