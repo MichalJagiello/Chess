dcef3195228dde8e3bc8d7f493a3338a69c69449
Jan Kaliszewski <zuo@kaliszewski.net>
2016-12-01 18:43:54 +0100

    Some cleanups and refactorings

--- chess/session.py
+++ chess/session.py
@@ -1,16 +1,13 @@
-from chess.board_loc import (
-    Board,
-    Location,
-)
-from chess.player import Player
+from chess.board_loc import Board
 from chess.move import MoveFactory
+from chess.player import Player
 
 
 class Session(object):
 
     def __init__(self, white_name, black_name):
         self.last_move = None
-        self.is_white_move = True
+        self.is_white_turn = True
         self.players = {
             True: Player(white_name),
             False: Player(black_name)
@@ -20,22 +17,14 @@ class Session(object):
 
     @property
     def current_player(self):
-        return self.players[self.is_white_move]
+        return self.players[self.is_white_turn]
 
     def setup(self):
         self.board.setup()
 
     def do_move(self, move_label):
-        """
-
-        :param player:
-        :param src_label:
-        :param dst_label:
-        :return: None
-        :raise: IllegalMoveError
-        """
 
         move = self._mover_factory.create(self, move_label)
         move.execute()
         self.last_move = move
-        self.is_white_move = not self.is_white_move
+        self.is_white_turn = not self.is_white_turn
