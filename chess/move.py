80e28f9c2f71b6d9f2920dfd6b874c140072d79c
Andrzej Dębicki <Andrzej Dbicki>
2016-12-01 13:37:03 +0100

    Added few checks.

--- chess/move.py
+++ chess/move.py
@@ -28,8 +28,11 @@ class NormalMove(Move):
         self._session = session
         # raise an exception, when any of those fields are out
         # of board's range
-        self.src_loc = self._session.board[self.src]
-        self.dst_loc = self._session.board[self.dst]
+        self.src_piece = self._session.board[self.src]
+        self.dst_piece = self._session.board[self.dst]
+        self._check_src_not_empty(self.src_piece)
+        self._check_if_player_owns_src_piece(self.src_piece)
+        self.route = self.src_piece.get_route()
 
     def get_vector(self):
         return self.src.get_vector(self._dst_label)
@@ -40,6 +43,15 @@ class NormalMove(Move):
     def execute(self):
         pass
         # TODO
+
+    @staticmethod
+    def _check_src_not_empty(self, piece):
+        if piece is None:
+            raise IllegalMoveError('Source location does not contain any figure.')
+
+    def _check_if_player_owns_src_piece(self, piece):
+        if piece.is_white != self._session.is_white_move:
+            raise IllegalMoveError('You tried to move not your piece.')
 
     def _check_src_dst_are_different(self):
         if not any(self.get_vector()):
