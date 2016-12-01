df13c5bb7baca61944ad7de9a7af3bdd4030f874
Jan Kaliszewski <zuo@kaliszewski.net>
2016-12-01 19:05:35 +0100

    game/session: 8-queens-related adjustments + QueensPuzzleSession stub

--- chess/game.py
+++ chess/game.py
@@ -1,42 +1,75 @@
 import sys
 
 from chess.drawer import Drawer
-from chess.exceptions import IllegalMoveError
-from chess.session import Session
+from chess.exceptions import UserError
+from chess.session import (
+    ChessGameSession,
+    QueensPuzzleSession,
+)
 
 
 class ChessMajsterGame(object):
 
-    COORD_SEPARATOR = ' '
-    EXIT_GAME_CMD = 'exit'
-    EXIT_STATUS = 'Game has been quit by user.'
+    _action_item_separator = ' '
+    _exit_game_cmd = 'exit'
+    _exit_msg = 'Game has been quit by the user.'
 
     def __init__(self):
-        self.drawer = None
+        self.drawer = Drawer()
         self.session = None
 
     def run(self):
+        self.welcome()
+        session_runner = self.choose_session_runner()
+        session_runner()
+        self.good_bye()
+
+    def welcome(self):
         print "#############################"
         print "# Welcome to Chess Majster! #"
         print "#############################\n"
-        print "Type {0} any time to quit game.".format(self.EXIT_GAME_CMD)
+        print ("Type {0} (or Ctrl+D, or Ctrl+C) at any time "
+               "to quit the game.".format(self._exit_game_cmd))
+
+    def good_bye(self):
+        print '\nGame finished. Good bye! :-)'
+
+    def choose_session_runner(self):
+        while True:
+            print '\nPlease, choose the kind of session:'
+            print '1) chess game'
+            print '2) queens puzzle'
+            print
+            choice = self._input('Your choice:')
+            if choice == '1':
+                return self.run_chess_game
+            elif choice == '2':
+                return self.run_queens_puzzle
+            else:
+                print '1 or 2 expected...\n'
+
+    def run_chess_game(self):
+        print '\nStarting a chess game...\n'
         white_player_name = self._get_player_name('White')
         black_player_name = self._get_player_name('Black')
-        self.session = Session(white_player_name,  black_player_name)
+        self.session = ChessGameSession(white_player_name,  black_player_name)
         self.session.setup()
-        self.drawer = Drawer()
+        self._play_turns()
+
+    def run_queens_puzzle(self):
+        print '\nStarting the 8-queens puzzle...'
+        self.session = QueensPuzzleSession(player_name='Puzzle Detective')
+        self.session.setup()
         self._play_turns()
 
     def _get_player_name(self, player_color):
-        print "Enter name for Player {0!s}:".format(str(player_color))
         while True:
-            user_input = raw_input()
-            if user_input.lower() == self.EXIT_GAME_CMD:
-                sys.exit(self.EXIT_STATUS)
-            if not user_input:
-                print "You did not enter proper name."
-            else:
-                return user_input
+            player_name = self._input(
+                'Please, enter the name for the '
+                'player {0}:'.format(player_color))
+            if player_name:
+                return player_name
+            print 'You did not enter a proper name.'
 
     def _play_turns(self):
         # flag indicates weather it is a new turn and the board should
@@ -45,31 +78,31 @@ class ChessMajsterGame(object):
         while True:
             if is_new_turn:
                 self.drawer.show(self.session)
-            if self.session.is_white_turn:
-                which_player = 'White'
-            else:
-                which_player = 'Black'
             try:
-                self.session.do_move(self._get_coords(which_player))
-            except IllegalMoveError as exc:
+                self.session.act(self._get_action_spec_list())
+            except UserError as exc:
                 is_new_turn = False
                 print exc
             else:
                 is_new_turn = True
 
-    def _get_coords(self, which_player):
+    def _get_action_spec_list(self):
+        player_name = self.session.current_player.name
+        player_color = 'White' if self.session.is_white_turn else 'Black'
         while True:
-            print "{0} player turn".format(which_player)
-            print "Enter your move's coordinates separated by space:"
-            user_input = raw_input()
-            if user_input.lower() == self.EXIT_GAME_CMD:
-                sys.exit(self.EXIT_STATUS)
-            else:
-                try:
-                    src, dst = user_input.split(self.COORD_SEPARATOR)
-                except ValueError:
-                    return [user_input]
-                # except ValueError:
-                #     print "You did not enter two proper coordinates."
-                #     continue
-                return [src, dst]
+            raw_action = self._input(
+                '{} ({}), please enter your action:'
+                .format(player_name, player_color))
+            return map(str.strip, raw_action.split(self._action_item_separator))
+
+    def _input(self, msg=None):
+        if msg:
+            print msg,
+        try:
+            user_input = raw_input().strip().lower()
+            if user_input.lower() == self._exit_game_cmd:
+                raise EOFError
+        except (EOFError, KeyboardInterrupt):
+            print '\n\n', self._exit_msg
+            sys.exit()
+        return user_input
--- chess/session.py
+++ chess/session.py
@@ -5,26 +5,72 @@ from chess.player import Player
 
 class Session(object):
 
-    def __init__(self, white_name, black_name):
-        self.last_move = None
-        self.is_white_turn = True
-        self.players = {
-            True: Player(white_name),
-            False: Player(black_name)
-        }
+    def __init__(self, player_name):
         self.board = Board()
-        self._mover_factory = MoveFactory()
+        self.players = {
+            True: Player(player_name),
+        }
+        self.is_white_turn = True
 
     @property
     def current_player(self):
         return self.players[self.is_white_turn]
 
     def setup(self):
+        raise NotImplementedError
+
+    def act(self, action_spec_list):
+        raise NotImplementedError
+
+
+class ChessGameSession(Session):
+
+    def __init__(self, white_player_name, black_player_name):
+        super(ChessGameSession, self).__init__(white_player_name)
+        self.players[False] = Player(black_player_name)
+        self.last_move = None
+        self._move_factory = MoveFactory()
+
+    def setup(self):
         self.board.setup()
 
-    def do_move(self, move_label):
+    def act(self, action_spec_list):
+        self._do_move(move_spec=action_spec_list)
 
-        move = self._mover_factory.create(self, move_label)
+    def _do_move(self, move_spec):
+        move = self._move_factory.create(self, move_spec)
         move.execute()
         self.last_move = move
         self.is_white_turn = not self.is_white_turn
+
+
+class PuzzleSession(Session):
+
+    """
+    The base class for puzzle sessions.
+    """
+
+
+class QueensPuzzleSession(PuzzleSession):
+
+    """
+    The 8-queen puzzle session class.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(QueensPuzzleSession, self).__init__(*args, **kwargs)
+        self._queen_count = 0
+        #self._queen_resolver = 
+
+    def setup(self):
+        pass
+
+    def act(self, action_spec_list):
+        TODO
+        # try:
+        #     [loc_label] = action_spec_list
+        # except (ValueError, :
+        # self._set_queen(
+
+    def _set_queen(self, dst):
+        TODO
