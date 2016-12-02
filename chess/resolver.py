6859fc2f40d76ce4d3644bdf76c941c18c7f375a
mariusz <pythonms@gmail.com>
2016-12-02 08:40:51 +0100

    QueenResolver

--- chess/resolver.py
+++ chess/resolver.py
@@ -3,16 +3,22 @@
 class QueenResolver(object):
 
     def __init__(self):
-        self._reserved_fields = []
+        self._reserved_fields = set()
 
-    def is_usable_field(self, field):
+    def is_usable_field(self, loc):
         """
         Returns boolean value if field is usable to set.
-        """
-        pass
 
-    def reserve_field(self, field):
+        :param field:
+        :return: bool
+        """
+        return loc in self._reserved_fields
+
+    def reserve_field(self, loc):
         """
         Reserve given field
+
+        :param loc:
+        :return:
         """
-        pass
+        self._reserved_fields.update(loc)
